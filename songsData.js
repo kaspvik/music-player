60006574e5f5c8ebffefada697c65d1c5a69872c
Kasper Vikström <kasper.vik@hotmail.com>
2025-05-12 13:39:11 +0200

    add songs data and enhance playlist section styles

---
+++ songsData.js
@@ -0,0 +1,56 @@
+const songsData = [
+    {
+        id: 1,
+        title: "Bohemian Rhapsody",
+        artist: "Queen",
+        genre: "Rock"
+    },
+    {
+        id: 2,
+        title: "Billie Jean",
+        artist: "Michael Jackson",
+        genre: "Pop"
+    },
+    {
+        id: 3,
+        title: "Hotel California",
+        artist: "Eagles",
+        genre: "Rock"
+    },
+    {
+        id: 4,
+        title: "Shape of You",
+        artist: "Ed Sheeran",
+        genre: "Pop"
+    },
+    {
+        id: 5,
+        title: "Sweet Child O' Mine",
+        artist: "Guns N' Roses",
+        genre: "Rock"
+    },
+    {
+        id: 6,
+        title: "Bad Guy",
+        artist: "Billie Eilish",
+        genre: "Pop"
+    },
+    {
+        id: 7,
+        title: "Money",
+        artist: "Pink Floyd",
+        genre: "Rock"
+    },
+    {
+        id: 8,
+        title: "Stairway to Heaven",
+        artist: "Led Zeppelin",
+        genre: "Rock"
+    },
+    {
+        id: 9,
+        title: "Bad Romance",
+        artist: "Lady Gaga",
+        genre: "Pop"
+    }
+];
